34a95d9d28d4ac88625cebab7477aa01dfd14e3d
mthtitumir <titumirgccc@gmail.com>
2023-08-10 23:49:14 +0600

    hello

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import Navbar from '@/components/Navbar/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/Footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
