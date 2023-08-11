c19884899d4e95ed3a597bdb17e669c0d2472a36
salman-hossen <salmanhossen>
2023-08-11 12:03:56 +0600

    commited by salman added all news section

--- src/components/homeComponents/AllNews/AllNews.jsx
+++ src/components/homeComponents/AllNews/AllNews.jsx
@@ -1,10 +1,66 @@
 import React from 'react';
 import fakeData from "../../../utils/news.json"
+import NewsCardThree from '@/components/newsCards/NewsCardThree';
+import NewsCardOne from '@/components/newsCards/NewsCardOne';
 
 const AllNews = () => {
+    const data1 = fakeData.slice(0,3)
+    const data2 = fakeData.slice(3,6)
+    const data3 = fakeData.slice(0,10)
+
     return (
-        <div>
-            
+        <div className='flex justify-between'>
+            <div>
+                <h1>Sports</h1>
+                <div>
+                    {
+                        data1.map(item=> <NewsCardThree
+                        key={item.id}
+                        item={item}
+                        ></NewsCardThree>)
+                    }
+                </div>
+                <h1>Business</h1>
+                <div>
+                {
+                        data2.map(item=> <NewsCardThree
+                        key={item.id}
+                        item={item}
+                        ></NewsCardThree>)
+                    }
+                </div>
+            </div>
+            <div>
+                <h1>Sports</h1>
+                <div>
+                    {
+                        data1.map(item=> <NewsCardThree
+                        key={item.id}
+                        item={item}
+                        ></NewsCardThree>)
+                    }
+                </div>
+                <h1>Business</h1>
+                <div>
+                {
+                        data2.map(item=> <NewsCardThree
+                        key={item.id}
+                        item={item}
+                        ></NewsCardThree>)
+                    }
+                </div>
+            </div>
+            <div>
+            <h1>Trending</h1>
+                <div>
+                {
+                        data3.map(item=> <NewsCardOne
+                        key={item.id}
+                        item={item}
+                        ></NewsCardOne>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
