87b802247419bffc437bc5db22a35cdccfda183d
somaiya shahin <somaiyashahin484@gmail.com>
2023-08-11 13:02:06 +0600

    2nd commit

--- src/components/homeComponents/WeekNews/WeekNews.jsx
+++ src/components/homeComponents/WeekNews/WeekNews.jsx
@@ -10,42 +10,40 @@ import Image from 'next/image'
 
 
 const WeekNews = () => {
-    const data = fakeData.slice(0, 8);
+    const data = fakeData.slice(0, 7);
     const data1 = fakeData.slice(0, 1);
     const data2 = fakeData.slice(0, 4);
 
 
     return (
         <div className='mt-5 md:mb-4'>
-            <div className=''>
-                <div className='grid md:grid-cols-3'>
-                    <div className=''>
-                        <Headline headline={"Weekly News"} seeMore={"see more"} />
+            <div className='grid md:grid-cols-3 w-[97%] mx-auto gap-7'>
+                <div className=''>
+                    <Headline headline={"Weekly News"} seeMore={"see more"} />
 
-                        {
-                            data2.map(item => <NewsCardThree
-                                key={item.id}
-                                item={item}
-                            ></NewsCardThree>)
-                        }
-                    </div>
-                    <div className=''>
-                        <div className=''>
-                            <Headline headline={"Weekly News"} seeMore={"see more"} />
-                            {
-                                data.map(item => <NewsCardOne
-                                    key={item.id}
-                                    item={item}
-                                ></NewsCardOne>)
-                            }
-                        </div>
-                    </div>
+                    {
+                        data2.map(item => <NewsCardThree
+                            key={item.id}
+                            item={item}
+                        ></NewsCardThree>)
+                    }
                 </div>
                 <div className=''>
+                    <div className='lg:h-20'>
+                        <Headline headline={"Weekly News"} seeMore={"see more"} />
+                        {
+                            data.map(item => <NewsCardOne
+                                key={item.id}
+                                item={item}
+                            ></NewsCardOne>)
+                        }
+                    </div>
+                </div>
+                <div className='mt-6'>
                     <Image
-                        src="https://i.ibb.co/t8L980p/New-Project-1.png"
+                        src="https://i.ibb.co/92BFdLM/5190537997-89cf7af050-c.jpg"
                         height={400}
-                        width={600}
+                        width={300}
                         alt="Picture of the author"
                     >
                     </Image>
--- src/components/newsCards/NewsCardThree.jsx
+++ src/components/newsCards/NewsCardThree.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const NewsCardThree = ({ item }) => {
     return (
-        <div className="card card-side">
+        <div className="card card-side bg-gray-100 rounded-none my-4">
             <div className='w-1/3 relative'>
                 <Image width={460} height={510} src={item?.img} alt='banner' />
                 <div className='absolute top-0 right-0 px-4 py-1 bg-black bg-opacity-70'>
@@ -11,7 +11,7 @@ const NewsCardThree = ({ item }) => {
                 </div>
             </div>
             <div className="w-2/3 ml-3">
-                <h2 className="text-2xl ">{item?.text}!</h2>
+                <h2 className="text-xl underline ">{item?.text}!</h2>
                 <p>{item?.published_date}</p>
             </div>
         </div>
