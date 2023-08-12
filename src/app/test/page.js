bace3ffcdcb52eafdb2cb2ce0ece4e60746ea39a
suhanur rohoman sumon <sumonsuhanurrohoman@gmail.com>
2023-08-11 21:51:16 -0400

    mongodb cunnected

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const Test = () => {
+const Test = async () => {
+  const response = await fetch("http://localhost:3000/api/all-news")
+  const data = await response.json()
+  console.log(data)
   return (
     <div>Test herer
-      <h1>Hello from test one</h1>
+      <h1>{data[0].title}</h1>
       <h2>test2 likheche</h2>
     </div>
   )
