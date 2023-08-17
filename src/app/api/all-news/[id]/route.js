ac2139451a94d88e1bff10f6eee75b12023fa660
suhanur rohoman sumon <sumonsuhanurrohoman@gmail.com>
2023-08-17 13:23:22 -0400

    single data added

--- src/app/api/all-news/[id]/route.js
+++ src/app/api/all-news/[id]/route.js
@@ -2,8 +2,11 @@ import DbConnect from "@/services/DbConnect";
 import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
 
-export const GET = async ({ params }) => {
-    const id = (params.id)
+export const GET = async (request,{ params }) => {
+    const id =  (params.id)
+    console.log('Params:', params);
+    console.log('ID:', id);
+    
     const db = await DbConnect();
     const allNews = db.collection('all-news');
     const query = { _id: new ObjectId(id) }
