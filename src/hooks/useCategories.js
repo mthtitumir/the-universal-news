f51c93ac839bef15e049e973d6945049e677bd12
mthtitumir <titumirgccc@gmail.com>
2023-08-18 19:37:33 +0600

    local host replaced

--- src/hooks/useCategories.js
+++ src/hooks/useCategories.js
@@ -1,6 +1,6 @@
 export const categories = async (category) => {
-  const res = await fetch(`http://localhost:3000/api/category?category=${category}`)
-  if(!res.ok) {
+  const res = await fetch(`https://the-universal-news.vercel.app/api/category?category=${category}`)
+  if (!res.ok) {
     throw new Error('Failed to fetch data!')
   }
   return res.json();
