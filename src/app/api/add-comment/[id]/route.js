c8cf18b458078b4310fa27069abb24b8559e1195
suhanur rohoman sumon <sumonsuhanurrohoman@gmail.com>
2023-08-17 21:47:22 -0400

    comment degain updated

--- src/app/api/add-comment/[id]/route.js
+++ src/app/api/add-comment/[id]/route.js
@@ -6,20 +6,14 @@ export const POST = async (request, {params}) => {
     if (request.method === "POST") {
         try {
             const body = await request.json();
-            console.log(body);
             const id = params.id
-            console.log(id)
             const db = await DbConnect();
             const allNews = db.collection('all-news');
-
             const { comments, author,currentDate } = body;
-            
-
             const result = await allNews.updateOne(
                 { _id: new ObjectId(id) },
                 { $push: { comments: { id: Math.floor(Math.random() * 100000), author, text: comments, date: currentDate } } }
             );
-
             return NextResponse.json(result);
         } catch (error) {
             console.error("Error adding comment:", error);
--- src/app/api/all-news/[id]/route.js
+++ src/app/api/all-news/[id]/route.js
@@ -3,12 +3,18 @@ import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
 
 export const GET = async (request, { params }) => {
-    const id = params.id;
-    const db = await DbConnect();
-    const allNews = db.collection('all-news');
-    const query = { _id: new ObjectId(id) }
-    const result = await allNews.findOne(query)
-    return NextResponse.json(result);
+    try {
+        const id = params.id;
+        const db = await DbConnect();
+        const allNews = db.collection('all-news');
+        const query = { _id: new ObjectId(id) }
+        const result = await allNews.findOne(query)
+        return NextResponse.json(result);
+    }
+    catch (error) {
+        console.error("error for geting data", error);
+        NextResponse.json({ error: "eroor for geting data" });
+    }
 };
 
 
