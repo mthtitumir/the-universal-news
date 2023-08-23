9124a34051105a524b07c696abefd3860da42798
mthtitumir <titumirgccc@gmail.com>
2023-08-23 23:21:13 +0600

    games error

--- src/app/(newsLayout)/sports/cricket/page.jsx
+++ src/app/(newsLayout)/sports/cricket/page.jsx
@@ -2,13 +2,12 @@ import CricInfo from "@/components/CricInfo/CricInfo";
 import LiveScoreSlider from "@/components/LiveScoreSlider/LiveScoreSlider";
 
 const Cricket = async() => {
-    const scoreUrl = await fetch('https://api.cricapi.com/v1/currentMatches?apikey=32c8bddb-0190-47ae-a781-61664a2f8783&offset=0')
-    const liveScores = await scoreUrl.json()
+    
     // console.log(liveScores);
     return (
         <div className="md:mt-11 container mx-auto">
             <div className=''>
-                <LiveScoreSlider liveScores={liveScores.data} />
+                <LiveScoreSlider />
             </div>
             <div>
                 <div>
--- src/components/SingleScore/SingleScore.jsx
+++ src/components/SingleScore/SingleScore.jsx
@@ -2,19 +2,20 @@
 import React from 'react';
 import Image from 'next/image';
 const SingleScore = ({ singleScore }) => {
-    const { teams, teamInfo, date, score, name, status, matchType } = singleScore;
+    const { teams, teamInfo, score, name, status, matchType } = singleScore;
+    console.log(singleScore);
     return (
         <div className="shadow-xl p-5 border my-3">
             <div className="flex flex-col gap-1">
                 {/* match title  */}
                 <div className="flex justify-between items-center gap-2">
-                    <p className='text-xs'>{name.length<46?name:`${name.slice(0,45)}...`}</p>
+                    <p className='text-xs'>{name.length < 46 ? name : `${name.slice(0, 45)}...`}</p>
                     <p className='text-sm badge badge-success'>{matchType ? matchType : 'odi'}</p>
                 </div>
                 {/* score  */}
                 <div className='flex flex-col gap-2'>
                     <div className='flex items-center'>
-                        <Image src={teamInfo[1].img} height='20' width='20' className='rounded-full' alt='team img' />
+                        {/* <Image src={teamInfo[1].img ? teamInfo[1].img : ""} height='20' width='20' className='rounded-full' alt='team img' /> */}
                         <p className='ml-4'>{teamInfo[1].shortname ? teamInfo[1].shortname : teams[0]}</p>
                         <div className='flex ml-3'>
                             <p>{score[0]?.r}/</p>
