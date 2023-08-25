1663aeb93a2578da9fe4aa00446c3cf3b902238d
Shakil57375 <shakil57375@gmail.com>
2023-08-25 20:27:05 +0600

    UI follised of Rock Paaper Scissor game

--- src/app/(newsLayout)/playground/games/RockPaperScissor/page.js
+++ src/app/(newsLayout)/playground/games/RockPaperScissor/page.js
@@ -1,10 +1,14 @@
-"use client"
+"use client";
 /* eslint-disable react/no-unescaped-entities */
+import React, { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import Image from "next/image"; // Import Next.js Image component
 
-import React, { useState } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
-
-const options = ['rock', 'paper', 'scissors'];
+const options = [
+  "https://i.ibb.co/GCwn4cb/rock-image.png", // Replace with the actual image URLs
+  "https://i.ibb.co/8xZpfBf/paper-image.png",
+  "https://i.ibb.co/CtqPDXj/scissors-image.png",
+];
 
 const getRandomOption = () => {
   const randomIndex = Math.floor(Math.random() * options.length);
@@ -14,26 +18,29 @@ const getRandomOption = () => {
 const RockPaperScissors = () => {
   const [userChoice, setUserChoice] = useState(null);
   const [computerChoice, setComputerChoice] = useState(null);
-  const [result, setResult] = useState('');
+  const [result, setResult] = useState("");
   const [userScore, setUserScore] = useState(0);
   const [computerScore, setComputerScore] = useState(0);
 
   const playGame = (userChoice) => {
     const computerChoice = getRandomOption();
 
-    let gameResult = '';
+    let gameResult = "";
     if (userChoice === computerChoice) {
-      gameResult = 'It\'s a tie!';
-    } else if (
-      (userChoice === 'rock' && computerChoice === 'scissors') ||
-      (userChoice === 'paper' && computerChoice === 'rock') ||
-      (userChoice === 'scissors' && computerChoice === 'paper')
-    ) {
-      gameResult = 'You win!';
-      setUserScore(userScore + 1);
+      gameResult = "It's a tie!";
     } else {
-      gameResult = 'Computer wins!';
-      setComputerScore(computerScore + 1);
+      // Determine the result based on image URLs
+      if (
+        (userChoice === options[0] && computerChoice === options[2]) ||
+        (userChoice === options[1] && computerChoice === options[0]) ||
+        (userChoice === options[2] && computerChoice === options[1])
+      ) {
+        gameResult = "You win!";
+        setUserScore(userScore + 1);
+      } else {
+        gameResult = "Computer wins!";
+        setComputerScore(computerScore + 1);
+      }
     }
 
     setUserChoice(userChoice);
@@ -47,16 +54,28 @@ const RockPaperScissors = () => {
   const resetGame = () => {
     setUserChoice(null);
     setComputerChoice(null);
-    setResult('');
+    setResult("");
+    setUserScore(0);
+    setComputerScore(0);
   };
 
   return (
-    <div className="flex flex-col items-center">
-      <Toaster position="top-right" />
-
-      <h1 className="text-2xl font-bold mb-4">Rock, Paper, Scissors</h1>
+    <div className="flex flex-col items-center h-screen px-2">
+      <div className="flex items-center flex-col justify-center mt-10 mb-8">
+        <h1 className="text-4xl font-bold mb-5 text-center">Rock, Paper, Scissors</h1>
+        <div className=" ">
+          <div className="flex items-center gap-2">
+            <p className="text-white rounded-xl p-4 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 ">
+              You : {userScore}
+            </p>
+            <p className="text-white rounded-xl p-4 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 ">
+              Computer : {computerScore}
+            </p>
+          </div>
+        </div>
+      </div>
       <div className="mb-4">
-        <p className="mb-2">
+        {/* <p className="mb-2">
           How to Win:
           <br />
           Rock beats Scissors
@@ -64,27 +83,76 @@ const RockPaperScissors = () => {
           Scissors beats Paper
           <br />
           Paper beats Rock
-        </p>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2" onClick={() => playGame('rock')}>
-          Rock
-        </button>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2" onClick={() => playGame('paper')}>
-          Paper
-        </button>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => playGame('scissors')}>
-          Scissors
-        </button>
+        </p> */}
+        {/* Display images for options and handle click events */}
+        <div className="flex items-center justify-between gap-2 lg:gap-10">
+          <div className="lg:h-28 h-24 flex items-center justify-center lg:w-32 w-28 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-xl">
+            <Image
+              src={options[0]}
+              alt="Rock"
+              width={80}
+              height={800}
+              className="cursor-pointer "
+              onClick={() => playGame(options[0])}
+            />
+          </div>
+          <div className="lg:h-28 h-24 flex items-center justify-center lg:w-32 w-28 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-xl">
+            <Image
+              src={options[1]}
+              alt="Paper"
+              width={60}
+              height={60}
+              className="cursor-pointer"
+              onClick={() => playGame(options[1])}
+            />
+          </div>
+          <div className="lg:h-28 h-24 flex items-center justify-center lg:w-32 w-28 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-xl">
+            <Image
+              src={options[2]}
+              alt="Scissors"
+              width={80}
+              height={80}
+              className="cursor-pointer"
+              onClick={() => playGame(options[2])}
+            />
+          </div>
+        </div>
       </div>
       {userChoice && computerChoice && (
-        <div>
-          <p>Your choice: {userChoice}</p>
-          <p>Computer's choice: {computerChoice}</p>
-          <p>{result}</p>
-          <p>Your Score: {userScore}</p>
-          <p>Computer Score: {computerScore}</p>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={resetGame}>
-            Reset
-          </button>
+        <div className="w-full px-8">
+          <div className="flex flex-col justify-between lg:gap-10 gap-4 items-center w-full">
+            <div className="flex  mx-auto justify-center gap-10 items-center w-full">
+              <div>
+                <p className="text-xl font-bold ml-8"> You</p>
+                <div className="lg:h-28 h-24 flex  items-center justify-center lg:w-32 w-28 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-xl">
+                  <Image
+                    src={userChoice}
+                    alt="User's choice"
+                    width={80}
+                    height={80}
+                  />
+                </div>
+              </div>
+              <p className="text-xl font-semibold text-red-600">{result}</p>
+              <div>
+                <p className="text-xl font-bold">Computer</p>
+                <div className="lg:h-28 h-24 flex  items-center justify-center lg:w-32 w-28 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-xl">
+                  <Image
+                    src={computerChoice}
+                    alt="Computer's choice"
+                    width={80}
+                    height={80}
+                  />
+                </div>
+              </div>
+            </div>
+            <button
+              className="from-pink-500 via-red-500 to-yellow-500 rounded-xl text-white font-bold py-2 px-4 "
+              onClick={resetGame}
+            >
+              Reset
+            </button>
+          </div>
         </div>
       )}
     </div>
--- src/app/(newsLayout)/playground/games/memory-game/singleCardComp/SingleCard.js
+++ src/app/(newsLayout)/playground/games/memory-game/singleCardComp/SingleCard.js
@@ -1,34 +1,31 @@
-import './SingleCard.css'
-import Image from 'next/image';
-
-
+import "./SingleCard.css";
+import Image from "next/image";
 
 export default function SingleCard({ card, handleChoice, flipped, disable }) {
-
-    const handleClick = () => {
-        if (!disable) {
-            handleChoice(card)
-        }
+  const handleClick = () => {
+    if (!disable) {
+      handleChoice(card);
     }
-    return (
-        <div className='singleCard '>
-            <div className={flipped ? "flipped" : ""}>
-                <Image
-                    className='singleImg front'
-                    height={98}
-                    width={100}
-                    src={card.src}
-                    alt="front image"
-                ></Image>
-                <Image
-                    onClick={handleClick}
-                    className='singleImg back'
-                    height={300}
-                    width={100}
-                    src="https://i.ibb.co/PYK4qcd/cover.jpg"
-                    alt="front image"
-                ></Image>
-            </div>
-        </div>
-    )
+  };
+  return (
+    <div className="singleCard ">
+      <div className={flipped ? "flipped" : ""}>
+        <Image
+          className="singleImg front"
+          height={98}
+          width={100}
+          src={card.src}
+          alt="front image"
+        ></Image>
+        <Image
+          onClick={handleClick}
+          className="singleImg back"
+          height={300}
+          width={100}
+          src="https://i.ibb.co/PYK4qcd/cover.jpg"
+          alt="front image"
+        ></Image>
+      </div>
+    </div>
+  );
 }
