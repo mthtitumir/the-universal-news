63979b49e7ad72bf53fe95513318034ad1a27b3c
mthtitumir <titumirgccc@gmail.com>
2023-08-30 09:45:21 +0600

    jjj

--- src/app/api/add-news/route.js
+++ src/app/api/add-news/route.js
@@ -1,5 +1,4 @@
 import DbConnect from "@/services/DbConnect";
-// import DbConnect from "@/services/DbConnect";
 import { NextResponse } from "next/server";
 export const POST = async (request) => {
     if (request.method === "POST") {
@@ -16,7 +15,7 @@ export const POST = async (request) => {
             return NextResponse.json(result);
         } catch (error) {
             console.error("Error adding comment:", error);
-            NextResponse.json({ error: "Eroor ading data" });
+            NextResponse.json({ error: "Error adding data" });
         }
     } else {
         NextResponse.json({ message: "Method not allowed" });
