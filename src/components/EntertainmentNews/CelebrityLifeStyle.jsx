04ef74a9e7ea9feec663ebc5ca2b6a4805629a01
Shakil57375 <shakil57375@gmail.com>
2023-08-31 11:09:42 +0600

    tv-shows page added

---
+++ src/components/EntertainmentNews/CelebrityLifeStyle.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import celebrityData from "../../utils/lifeStyles.json";
+import SingleEntertainmentNews from './SingleEntertainmentNews';
+const CelebrityLifeStyle = () => {
+    const newses = celebrityData;
+    console.log(newses);
+  return (
+    <div>
+       <div>
+      <div className="relative">
+        <h2 className="text-4xl font-bold my-2 text-start">Life Style</h2>
+        <div className="w-24 h-1 bg-cyan-600 absolute left-0 top-11"></div>
+        <div className="flex flex-col gap-4 px-4">
+          {newses.map((news, index) => (
+            <SingleEntertainmentNews
+              key={news.id}
+              news={news}
+              newses = {newses}
+              index = {index}
+            ></SingleEntertainmentNews>
+          ))}
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default CelebrityLifeStyle
--- src/components/NextMovieBanner/NextMovieBanner.jsx
+++ src/components/NextMovieBanner/NextMovieBanner.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { AiFillStar } from 'react-icons/ai';
+import { AiFillStar } from "react-icons/ai";
 import { useEffect, useState } from "react";
 const NextMovieBanner = () => {
   const [movies, setMovies] = useState([]);
@@ -13,30 +13,39 @@ const NextMovieBanner = () => {
       .then((data) => setMovies(data.results));
   }, []);
   return (
-    <div className="flex flex-col gap-5">
-      {movies.map((movie) => (
-        <div key={movie.id} className="flex items-center justify-between gap-2 shadow-lg relative rounded-xl">
-          <div className="basis-1/2">
-            <Image
-              alt="banner image"
-              width={200}
-              height={200}
-              className=" z-10 rounded-xl"
-              src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-            ></Image>
-          </div>
-          <div className="basis-1/2 mt-3">
-            <h1 className="text-xl font-medium z-30">{movie.original_title}</h1>
-            <div className="flex items-center gap-1">
-              <AiFillStar className='text-yellow-400' />
-              <span>{movie.vote_average}</span>
+    <div>
+      <h1 className="text-3xl font-bold mb-2">Up Next</h1>
+
+      <div className="flex flex-col gap-5">
+        {movies.map((movie) => (
+          <div
+            key={movie.id}
+            className="flex items-center justify-between gap-2 shadow-lg relative rounded-xl"
+          >
+            <div className="basis-1/2">
+              <Image
+                alt="banner image"
+                width={200}
+                height={200}
+                className=" z-10 rounded-xl"
+                src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+              ></Image>
+            </div>
+            <div className="basis-1/2 mt-3">
+              <h1 className="text-xl font-medium z-30">
+                {movie.original_title}
+              </h1>
+              <div className="flex items-center gap-1">
+                <AiFillStar className="text-yellow-400" />
+                <span>{movie.vote_average}</span>
+              </div>
+            </div>
+            <div className="absolute top-0 right-0 bg-cyan-600 text-white p-1 rounded-bl-md z-20">
+              <p>{movie.release_date}</p>
             </div>
           </div>
-          <div className="absolute top-0 right-0 bg-cyan-600 text-white p-1 rounded-bl-md z-20">
-            <p>{movie.release_date}</p>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
