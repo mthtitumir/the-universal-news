ada30ef20752ad541a461f00a88c9a434c55e26d
Shakil57375 <shakil57375@gmail.com>
2023-08-31 15:09:39 +0600

    smooth scrooler added on potcast

--- src/components/Trending/SingleCast.jsx
+++ src/components/Trending/SingleCast.jsx
@@ -1,15 +1,9 @@
-"use client";
-
-import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
+import React from "react";
 import Image from "next/image";
+import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
 
-const SingleCast = ({ TrendingCast, handleSrc }) => {
-  //   const [isPlaying, setIsPlaying] = useState(false);
-  const { title, image, src, host, release_date, viewers } = TrendingCast;
-
-  const togglePlayPause = () => {
-    setIsPlaying((prevState) => !prevState);
-  };
+const SingleCast = ({ TrendingCast, handleSrc, isPlaying, scrollToIframe }) => {
+  const { title, image } = TrendingCast;
 
   return (
     <div className="card w-full bg-base-100 shadow-xl relative group">
@@ -20,30 +14,31 @@ const SingleCast = ({ TrendingCast, handleSrc }) => {
           src={image}
           alt="book image"
           className="w-full h-full"
-        ></Image>
+        />
       </figure>
       <div className="card-body">
         <h2 className="lg:text-xl text-lg font-semibold">{title}</h2>
       </div>
       <div className="absolute top-[20%] left-1/2 hidden group-hover:block transition-colors duration-300 ease-in-out delay-100">
-        {/* {isPlaying ? ( */}
-        <AiFillPlayCircle
-          className="w-10 h-10 text-white cursor-pointer"
-          onClick={() => handleSrc(src, title)}
-        />
-        {/* ) : ( */}
-        {/* <AiFillPlayCircle
+        {isPlaying ? (
+          <AiFillPauseCircle
             className="w-10 h-10 text-white cursor-pointer"
-            onClick={togglePlayPause}
+            onClick={() => {
+              handleSrc(); // Pause by clearing the iframe source
+            }}
           />
-        )} */}
+        ) : (
+          <AiFillPlayCircle
+            className="w-10 h-10 text-white cursor-pointer"
+            onClick={() => {
+              handleSrc();
+              scrollToIframe(); // Scroll to the iframe when clicked
+            }}
+          />
+        )}
       </div>
     </div>
   );
 };
 
 export default SingleCast;
-
-/* 
-// <iframe width="560" height="315" src="https://www.youtube.com/embed/ihD8jA0eyis?si=JJALFREIQq49XrYU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-*/
