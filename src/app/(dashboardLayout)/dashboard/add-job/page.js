23fbacca2427cdbf4fbe46fe3143596f2324f032
suhanur rohoman sumon <sumonsuhanurrohoman@gmail.com>
2023-08-31 20:59:14 -0400

    add job added

--- src/app/(dashboardLayout)/dashboard/add-job/page.js
+++ src/app/(dashboardLayout)/dashboard/add-job/page.js
@@ -1,13 +1,15 @@
 "use client"
 import useAuth from '@/hooks/useAuth';
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const AddAJob = () => {
     const { user } = useAuth();
 
-    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    const { register, handleSubmit,control, formState: { errors }, reset } = useForm();
     const onSubmit = async (data) => {
         console.log(data);
         const id = Math.floor(Math.random() * 100000);
@@ -45,29 +47,29 @@ const AddAJob = () => {
                 <form onSubmit={handleSubmit(onSubmit)} className="card-body">
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text card-text-secondary">Title</span>
+                            <span className="label-text card-text-secondary">Job Title</span>
                         </label>
-                        <input type="text" placeholder="News title" {...register("title", { required: true })} className="input input-bordered" />
+                        <input type="text" placeholder="Job Title" {...register("title", { required: true })} className="input input-bordered" />
                     </div>
-                    {errors.title?.type === 'required' && <p className="text-red-500">title is required</p>}
+                    {errors.title?.type === 'required' && <p className="text-red-500">Job Title is required</p>}
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text card-text-secondary">Description</span>
+                            <span className="label-text card-text-secondary">jobDescription</span>
                         </label>
-                        <input type="text" placeholder="News description" {...register("description", { required: true })} className="input input-bordered" />
+                        <input type="text" placeholder="Job Description" {...register("description", { required: true })} className="input input-bordered" />
                     </div>
-                    {errors.description?.type === 'required' && <p className="text-red-500">description is required</p>}
+                    {errors.description?.type === 'required' && <p className="text-red-500">Job Description is required</p>}
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text card-text-secondary">Image Link</span>
+                            <span className="label-text card-text-secondary">Company Name</span>
                         </label>
-                        <input type="text" placeholder="News img link" {...register("img", { required: true })} className="input input-bordered" />
-                    </div>                    
+                        <input type="text" placeholder="News img link" {...register("companyName", { required: true })} className="input input-bordered" />
+                    </div>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text card-text-secondary">Author Name</span>
+                            <span className="label-text card-text-secondary">companyLogo</span>
                         </label>
-                        <input type="text" placeholder="author name" {...register("author")} defaultValue={user?.displayName} className="input input-bordered" />
+                        <input type="text" placeholder="author name" {...register("companyLogo")} className="input input-bordered" />
                     </div>
                     <div className="form-control">
                         <label className="label">
@@ -75,11 +77,48 @@ const AddAJob = () => {
                         </label>
                         <input type="text" placeholder="author email" {...register("email")} defaultValue={user?.email} readOnly className="input input-bordered" />
                     </div>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Job Location</span>
+                        </label>
+                        <select className='input input-bordered w-full' {...register("jobLocation", { required: true })}>
+                            <option className='my-2' value="">Select job Location </option>
+                            <option value="remote">Remote</option>
+                            <option value="onsite">Onsite</option>
+                        </select>
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">EmployMent Type</span>
+                        </label>
+                        <select className='input input-bordered w-full' {...register("employmentType", { required: true })}>
+                            <option value="">Select EmployMent Type </option>
+                            <option value="full-time">Fulltime</option>
+                            <option value="part-time">Part time</option>
+                        </select>
+                    </div>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text card-text-secondary">Add Tags</span>
+                            <span className="label-text">applicationDeadline</span>
                         </label>
-                        <input type="text" placeholder='Related keywords!'  {...register("keywords")} className="input input-bordered" />
+                        <Controller
+                            control={control}
+                            name="applicationDeadline"
+                            render={({ field }) => (
+                                <DatePicker
+                                    className="input input-bordered input-accent w-full"
+                                    {...field}
+                                    selected={field.value}
+                                    onChange={(date) => field.onChange(date)}
+                                    showMonthDropdown
+                                    showYearDropdown
+                                    dropdownMode="select"
+                                    placeholderText="applicationDeadline"
+                                    dateFormat="dd/MM/yyyy"
+                                    isClearable
+                                />
+                            )}
+                        />
                     </div>
                     <div className="form-control mt-6">
                         <button className="primary-btn">Add This News</button>
