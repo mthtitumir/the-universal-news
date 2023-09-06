15b23690df2922a9589997937c189b921e6b44b0
mthtitumir <titumirgccc@gmail.com>
2023-09-06 07:06:21 +0600

    employer home fixed

--- src/app/(dashboardLayout)/dashboard/employer/page.js
+++ src/app/(dashboardLayout)/dashboard/employer/page.js
@@ -1,78 +1,22 @@
-"use client"
-import DeleteNews from '@/components/DashboardComponents/DeleteNews';
-import useAuth from '@/hooks/useAuth';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import ReactApexChart from 'react-apexcharts';
-
-const EmployerOverview = () => {
-    const { user } = useAuth();
-    const [jobData, setJobData] = useState([]);
-    useEffect(() => {
-        const fetchData = async () => {
-            if (user) {
-                try {
-                    const response = await axios.get(`/api/getSingleJobData?email=${user.email}`);
-                    setJobData(response.data);
-                } catch (error) {
-                    console.error("Error fetching reporter news:", error);
-                }
-            }
-        };
-        fetchData();
-    }, [user])
-    const [applicationsData, setApplicationsData] = useState([]);
-    useEffect(() => {
-        const fetchData = async () => {
-            if (user) {
-                try {
-                    const response = await axios.get(`/api/getSingleAplications?email=${user.email}`);
-                    setApplicationsData(response.data);
-                } catch (error) {
-                    console.error("Error fetching reporter news:", error);
-                }
-            }
-        };
-        fetchData();
-    }, [user])
-    const chartData = {
-        labels: ['Total Job Postings', 'Total Applications'],
-        datasets: [
-            {
-                name: 'Data',
-                data: [jobData?.length, applicationsData?.length],
-            },
-        ],
-    };
-
-    const chartOptions = {
-        xaxis: {
-            categories: chartData.labels,
-        },
-    };
+const EmployerHome = () => {
     return (
-        <div>
-            <h1 className='text-center text-5xl text-cyan-500 mt-5'>well come back </h1>
-            <p className='text-center text-4xl text-cyan-500 mt-5'>{user?.displayName}</p>
-            <div className="stats shadow flex justify-between">
-
-                <div className="stat text-center mt-4">
-
-                    <div className="stat-title">total job posted</div>
-                    <div className="stat-value text-primary">{jobData.length}</div>
+        <div className="p-3">
+            <div className="stats shadow flex justify-between border-2 border-cyan-500 rounded-lg">
+                <div className="stat">
+                    <div className="stat-title">Total Likes</div>
+                    <div className="stat-value text-primary">25.6K</div>
                 </div>
-
-                <div className="stat text-center">
-
-                    <div className="stat-title">total aplications</div>
-                    <div className="stat-value text-secondary ">{applicationsData.length}</div>
+                <div className="stat">
+                    <div className="stat-title">Page Views</div>
+                    <div className="stat-value text-secondary">2.6M</div>
                 </div>
-            </div>
-            <div >
-                <ReactApexChart options={chartOptions} series={chartData.datasets} type="bar" height={350} />
+                <div className="stat">
+                    <div className="stat-title">Tasks done</div>
+                    <div className="stat-value">86%</div>
+                </div>
             </div>
         </div>
     );
 };
 
-export default EmployerOverview;
+export default EmployerHome;
