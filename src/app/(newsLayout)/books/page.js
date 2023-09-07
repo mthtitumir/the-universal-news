8be5e527d07664fd96b39c59df1e8e0119af0312
salman-hossen <salmanhossen>
2023-09-07 13:33:48 +0600

    books left ber added

--- src/app/(newsLayout)/books/page.js
+++ src/app/(newsLayout)/books/page.js
@@ -1,11 +1,20 @@
 import BooksRightBar from '@/components/Books/BooksRightBar';
+import BooksCategory from '@/components/Books/booksComponent/BooksCategory';
+import bookCategories  from "../../../utils/booksCategory.json";
+
 
 const Books = () => {
+    console.log(bookCategories);
     return (
-        <div className='flex-row md:flex c-auto'>
+        <div className='flex-row md:flex c-auto gap-4'>
             {/* left bar */}
-            <div className='md:w-[20%]'>
-                books left bar
+            <div style={{ borderRight: '1px solid #000' }} className='md:w-[20%] mt-10 pl-9'>               
+                {
+                    bookCategories.map(data => <BooksCategory
+                    key={data?.id}
+                    data={data}
+                    ></BooksCategory>)
+                }
             </div>
 
             {/* right bar */}
