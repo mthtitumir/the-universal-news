23f9105fe7c4a6ac752619c1e8363451f8138989
mthtitumir <titumirgccc@gmail.com>
2023-09-14 03:52:09 +0600

    employer api fix

--- src/app/api/apply-job/route.js
+++ src/app/api/apply-job/route.js
@@ -3,14 +3,11 @@ import { NextResponse } from "next/server";
 export const POST = async (request) => {
     if (request.method === "POST") {
         try {
-            const body = await request.json();
+            const applicationData = await request.json();
+            console.log(applicationData);
             const db = await DbConnect();
             const appliedJobs = db.collection('job-applications');
-            const { jobId, resume, coverLetter, employerEmail, userEmail } = body;
-            const doc = {
-                jobId, resume, coverLetter, employerEmail, userEmail
-            }
-            const result = await appliedJobs.insertOne(doc);
+            const result = await appliedJobs.insertOne(applicationData);
             return NextResponse.json(result);
         } catch (error) {
             console.error("Error adding comment:", error);
