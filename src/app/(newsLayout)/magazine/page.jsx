d0641c339da8ece2db6a88c59ca4d0e572d5fa70
mthtitumir <titumirgccc@gmail.com>
2023-09-22 22:55:27 +0600

    magazine single page

--- src/app/(newsLayout)/magazine/page.jsx
+++ src/app/(newsLayout)/magazine/page.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import SingleMagazine from '@/components/Magazine/SingleMagazine';
 import { GetAllMagazinesFromDB } from '@/services/GetAllNewsFromDB';
+import Advertisement from '@/components/EntertainmentNews/Advertisement';
 
 const Magazine = async () => {
     const magazineData = await GetAllMagazinesFromDB();
@@ -10,7 +11,8 @@ const Magazine = async () => {
             <div className='flex lg:flex-row flex-col-reverse items-center justify-between gap-4 w-full py-10'>
                 <div className='lg:basis-1/2 basis-full'>
                     <h1 className='font-bold text-4xl'>The Kind On The Night Shift</h1>
-                    <p className='text-2xl py-10'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br /> Recusandae perspiciatis, voluptate assumenda consectetur dicta neque!</p>
+                    <p className='text-2xl py-10'></p>
+                    <p className='text-lg'>I have ever had lasted less than 72 hours in 2012. In 2023, I am still building a non-streaming music collection, shelling out hard cash for</p>
                     <small>2023-09-19</small>
                 </div>
                 <div className='lg:basis-1/2 basis-full'>
@@ -24,7 +26,7 @@ const Magazine = async () => {
                 </div>
 
             </div>
-            <div className='flex justify-center border-t-2 border-black py-8'>
+            <div className='flex justify-center border-t-2 border-black pt-8'>
                 <div className='w-full border-black border-r-2 hover:scale-105 duration-500'>
                     <Image
                         className="rounded-lg mx-auto"
@@ -35,8 +37,8 @@ const Magazine = async () => {
                     />
                     <div className='px-14'>
                         <p>The magazine</p>
-                        <p className='py-4 text-3xl font-bold'>The Kind On The Night Shift</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, dolore ea libero ipsa omnis animi nesciunt eos eaque maiores sint?</p>
+                        <p className='py-4 text-3xl font-bold'>The Benefits of Meditation for Mind and Body</p>
+                        <p>The only music-streaming account I have ever had lasted less than 72 hours in 2012. In 2023, I am still building a non-streaming music collection</p>
                     </div>
                 </div>
                 <div className='w-full border-black border-r-2 hover:scale-105 duration-500'>
@@ -49,8 +51,8 @@ const Magazine = async () => {
                     />
                     <div className='px-14'>
                         <p>The magazine</p>
-                        <p className='py-4 text-3xl font-bold'>The Kind On The Night Shift</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, dolore ea libero ipsa omnis animi nesciunt eos eaque maiores sint?</p>
+                        <p className='py-4 text-3xl font-bold'>Exploring the science behind meditation</p>
+                        <p>I am still building a non-streaming music collection, shelling out hard cash for what the streaming industry has convinced consumers should be</p>
                     </div>
                 </div>
                 <div className='w-full hover:scale-105 duration-500'>
@@ -63,16 +65,16 @@ const Magazine = async () => {
                     />
                     <div className='px-14'>
                         <p>The magazine</p>
-                        <p className='py-4 text-3xl font-bold'>The Kind On The Night Shift</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, dolore ea libero ipsa omnis animi nesciunt eos eaque maiores sint?</p>
+                        <p className='py-4 text-3xl font-bold'>The only music-streaming account</p>
+                        <p>I am still building a non-streaming music collection, shelling out hard cash for what the streaming industry has convinced consumers should be</p>
                     </div>
                 </div>
 
             </div>
             <div className='hlx'></div>
-            <div className='grid md:grid-cols-10'>
+            <div className='grid md:grid-cols-10 gap-2 mt-8 '>
 
-                <div className='md:col-span-7'>
+                <div className='md:col-span-7 vly'>
                     {
                         magazineData?.map(data => <SingleMagazine
                             key={data?.id}
@@ -80,8 +82,10 @@ const Magazine = async () => {
                         ></SingleMagazine>)
                     }
                 </div>
-                <div className='md:col-span-3'>
-
+                <div className='md:col-span-3 pt-5'>
+                    <Advertisement />
+                    <Advertisement />
+                    <Advertisement />
                 </div>
             </div>
         </div>
